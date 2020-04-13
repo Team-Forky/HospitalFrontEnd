dff84be3bb7ce875730f5d46e25a131347bff89d
Andrew <shifted6@gmail.com>
2020-04-13 11:38:27 -0700

    Created project HospitalProjectFrontEnd and started scaffolding. Proof-of-life completed.

---
+++ HospitalProjectFrontEnd/HospitalProjectFrontEnd/Models/Services/PatientService.cs
@@ -0,0 +1,26 @@
+﻿using HospitalProjectFrontEnd.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HospitalProjectFrontEnd.Models.Services
+{
+    public class PatientService : IPatientManager
+    {
+        public Task<List<Patient>> GetAllPatients()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Patient> GetPatientById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Patient>> GetPatientsByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
